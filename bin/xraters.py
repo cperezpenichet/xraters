7f990843d4967052faae381e9acf6e583c9dae05
Carlos M. Perez Penichet <carlos@stargazer.(none)>
2011-02-01 09:35:51 -0500

    Initial project creation with Quickly!

---
+++ bin/xraters.py
@@ -0,0 +1,114 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+### BEGIN LICENSE
+# This file is in the public domain
+### END LICENSE
+
+import sys
+import os
+import gtk
+
+# Check if we are working in the source tree or from the installed 
+# package and mangle the python path accordingly
+if os.path.dirname(sys.argv[0]) != ".":
+    if sys.argv[0][0] == "/":
+        fullPath = os.path.dirname(sys.argv[0])
+    else:
+        fullPath = os.getcwd() + "/" + os.path.dirname(sys.argv[0])
+else:
+    fullPath = os.getcwd()
+sys.path.insert(0, os.path.dirname(fullPath))
+
+from xraters import AboutXratersDialog, PreferencesXratersDialog
+from xraters.xratersconfig import getdatapath
+
+class XratersWindow(gtk.Window):
+    __gtype_name__ = "XratersWindow"
+
+    def __init__(self):
+        """__init__ - This function is typically not called directly.
+        Creation a XratersWindow requires redeading the associated ui
+        file and parsing the ui definition extrenally,
+        and then calling XratersWindow.finish_initializing().
+
+        Use the convenience function NewXratersWindow to create
+        XratersWindow object.
+
+        """
+        pass
+
+    def finish_initializing(self, builder):
+        """finish_initalizing should be called after parsing the ui definition
+        and creating a XratersWindow object with it in order to finish
+        initializing the start of the new XratersWindow instance.
+
+        """
+        #get a reference to the builder and set up the signals
+        self.builder = builder
+        self.builder.connect_signals(self)
+
+        #uncomment the following code to read in preferences at start up
+        #dlg = PreferencesXratersDialog.NewPreferencesXratersDialog()
+        #self.preferences = dlg.get_preferences()
+
+        #code for other initialization actions should be added here
+
+    def about(self, widget, data=None):
+        """about - display the about box for xraters """
+        about = AboutXratersDialog.NewAboutXratersDialog()
+        response = about.run()
+        about.destroy()
+
+    def preferences(self, widget, data=None):
+        """preferences - display the preferences window for xraters """
+        prefs = PreferencesXratersDialog.NewPreferencesXratersDialog()
+        response = prefs.run()
+        if response == gtk.RESPONSE_OK:
+            #make any updates based on changed preferences here
+            pass
+        prefs.destroy()
+
+    def quit(self, widget, data=None):
+        """quit - signal handler for closing the XratersWindow"""
+        self.destroy()
+
+    def on_destroy(self, widget, data=None):
+        """on_destroy - called when the XratersWindow is close. """
+        #clean up code for saving application state should be added here
+
+        gtk.main_quit()
+
+def NewXratersWindow():
+    """NewXratersWindow - returns a fully instantiated
+    XratersWindow object. Use this function rather than
+    creating a XratersWindow directly.
+    """
+
+    #look for the ui file that describes the ui
+    ui_filename = os.path.join(getdatapath(), 'ui', 'XratersWindow.ui')
+    if not os.path.exists(ui_filename):
+        ui_filename = None
+
+    builder = gtk.Builder()
+    builder.add_from_file(ui_filename)
+    window = builder.get_object("xraters_window")
+    window.finish_initializing(builder)
+    return window
+
+if __name__ == "__main__":
+    #support for command line options
+    import logging, optparse
+    parser = optparse.OptionParser(version="%prog %ver")
+    parser.add_option("-v", "--verbose", action="store_true", dest="verbose", help="Show debug messages")
+    (options, args) = parser.parse_args()
+
+    #set the logging level to show debug messages
+    if options.verbose:
+        logging.basicConfig(level=logging.DEBUG)
+        logging.debug('logging enabled')
+
+    #run the application
+    window = NewXratersWindow()
+    window.show()
+    gtk.main()
+
---
+++ xraters/AboutXratersDialog.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+### BEGIN LICENSE
+# This file is in the public domain
+### END LICENSE
+
+import sys
+import os
+import gtk
+
+from xraters.xratersconfig import getdatapath
+
+class AboutXratersDialog(gtk.AboutDialog):
+    __gtype_name__ = "AboutXratersDialog"
+
+    def __init__(self):
+        """__init__ - This function is typically not called directly.
+        Creation of a AboutXratersDialog requires redeading the associated ui
+        file and parsing the ui definition extrenally, 
+        and then calling AboutXratersDialog.finish_initializing().
+    
+        Use the convenience function NewAboutXratersDialog to create 
+        NewAboutXratersDialog objects.
+    
+        """
+        pass
+
+    def finish_initializing(self, builder):
+        """finish_initalizing should be called after parsing the ui definition
+        and creating a AboutXratersDialog object with it in order to finish
+        initializing the start of the new AboutXratersDialog instance.
+    
+        """
+        #get a reference to the builder and set up the signals
+        self.builder = builder
+        self.builder.connect_signals(self)
+
+        #code for other initialization actions should be added here
+
+def NewAboutXratersDialog():
+    """NewAboutXratersDialog - returns a fully instantiated
+    AboutXratersDialog object. Use this function rather than
+    creating a AboutXratersDialog instance directly.
+    
+    """
+
+    #look for the ui file that describes the ui
+    ui_filename = os.path.join(getdatapath(), 'ui', 'AboutXratersDialog.ui')
+    if not os.path.exists(ui_filename):
+        ui_filename = None
+
+    builder = gtk.Builder()
+    builder.add_from_file(ui_filename)    
+    dialog = builder.get_object("about_xraters_dialog")
+    dialog.finish_initializing(builder)
+    return dialog
+
+if __name__ == "__main__":
+    dialog = NewAboutXratersDialog()
+    dialog.show()
+    gtk.main()
+
